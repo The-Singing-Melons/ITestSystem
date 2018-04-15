89c49f0ed933269564386eb89329b5b97c5df95b
Lubomir Yonchev <infra1515@gmail.com>
2018-04-15 16:59:17 +0300

    Added Areas. Changes on User Area / HomeController/Index

---
+++ ITest/ITest.Web/Areas/User/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITest.Web.Areas.User.Controllers
+{
+    [Area("User")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
