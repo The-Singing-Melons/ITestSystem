8137815c469f63fa90a1ffd9446f8dd8904dfd56
Unknown <dimitar.petrov.311@gmail.com>
2018-04-17 17:41:18 +0300

    Working on Creating Tests Functionality. Admin area Added. Default Scafold Home Index page removed. Summernote Added.

---
+++ ITest/ITest.Web/Areas/Admin/Controllers/ManageController.cs
@@ -0,0 +1,46 @@
+﻿using ITest.Web.Areas.Admin.Models.ManageViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITest.Web.Areas.Admin.Controllers
+{
+    //[Authorize(Roles = "Administrator")]
+    [Area("Admin")]
+    public class ManageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult CreateTest()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult CreateTest(CreateTestViewModel model)
+        {
+            //if (validationsFail)
+            //{
+            //  return View(model);
+            //}
+
+            //When all fine
+
+            return RedirectToAction(nameof(this.Index));
+        }
+
+        public IActionResult LoadQuestion()
+        {
+            return PartialView("_LoadQuestionPartial");
+        }
+
+        public IActionResult LoadAnswer()
+        {
+            return PartialView("_LoadAnswerPartial");
+        }
+    }
+}
---
+++ ITest/ITest.Web/Areas/Admin/Models/ManageViewModels/CreateTestViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ITest.Web.Areas.Admin.Models.ManageViewModels
+{
+    public class CreateTestViewModel
+    {
+        public string TestName { get; set; }
+
+        public int RequestedTime { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public IEnumerable<CreateQuestonViewModel> Questions { get; set; }
+    }
+}
