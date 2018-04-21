f580e9579125350de26c24818ce027020955adb0
Lubomir Yonchev <infra1515@gmail.com>
2018-04-21 12:38:59 +0300

    Fixed test redirect. Now asyncrounous call to DB and redirect to TakeTest controller works.

--- ITest/ITest.Web/wwwroot/js/user-tests.js
+++ ITest/ITest.Web/wwwroot/js/user-tests.js
@@ -4,14 +4,17 @@
 
         var category = $('.active a').data('category').replace(/\s+/g, '+');
 
-        console.log(category);
         var url = '/user/home/GetRandomTest/' + category;
-
         $.ajax({
             type: "GET",
             url: url,
-            success: function (response) {
-                console.log(response);
+            success: function (data) {
+                window.location.href = data;
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                console.log(xhr);
+                alert(xhr.status);
+                alert(thrownError);
             }
         });
     })
