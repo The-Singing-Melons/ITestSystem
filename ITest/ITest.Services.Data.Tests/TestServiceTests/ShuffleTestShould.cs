7475ee3dde84369796d1adad65de48e3012333c4
Lubomir Yonchev <infra1515@gmail.com>
2018-05-04 20:51:18 +0300

    CreateTest fully tested

--- ITest/ITest.Services.Data.Tests/TestServiceTests/ShuffleTestShould.cs
+++ ITest/ITest.Services.Data.Tests/TestServiceTests/ShuffleTestShould.cs
@@ -163,5 +163,18 @@ namespace ITest.Services.Data.Tests.TestServiceTests
             // Assert
             Assert.AreNotEqual(testQuestionsDto, testDto.Questions);
         }
+
+        [TestMethod]
+        public void ThrowArgumentNull_WhenCalledWithInvalidParameter()
+        {
+            // Arrange
+            var sut = new TestService(userRepoMock.Object, testRepoMock.Object, questionRepoMock.Object,
+            answerRepoMock.Object, dataSaverMock.Object, mapperMock.Object, categoryRepoMock.Object,
+            randomMock.Object, shufflerMock.Object);
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => sut.ShuffleTest(null));
+        }
+
     }
 }
